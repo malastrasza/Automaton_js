586ab83106ad4612ca28f3c3ea10cb6b2dfe3d61
malastrasza <malastrasza@gmail.com>
2019-01-11 20:58:46 +0100

    microchanges

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-   function GameOfLife(width, height) {
-       this.boardWidth = width;
-       this.boardHeight = height;
+document.addEventListener("DOMContentLoaded", function () {
+    function GameOfLife(width, height) {
+        this.boardWidth = width;
+        this.boardHeight = height;
     }
+
+    const game = new GameOfLife(10, 10);
+
+    console.log(game);
+
+
 
 });
