2c5190226f71c4729c75d6ead208eb85fca75298
malastrasza <malastrasza@gmail.com>
2019-01-13 21:31:16 +0100

    slight changes

--- js/app.js
+++ js/app.js
@@ -2,12 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
     function GameOfLife(width, height) {
         this.boardWidth = width;
         this.boardHeight = height;
+        var board = document.getElementById('board')
     }
 
     const game = new GameOfLife(10, 10);
 
     console.log(game);
 
+    function createBoard() {
+
+    }
 
 
 });
